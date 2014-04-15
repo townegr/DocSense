40879c90e3ab08e79f42930d6b8b13c400c79786
Greg Towne <townegr@yahoo.com>
2014-04-14 23:38:06 -0400

    incomplete update of encounter feature spec

--- app/controllers/encounters_controller.rb
+++ app/controllers/encounters_controller.rb
@@ -1,2 +1,29 @@
 class EncountersController < ApplicationController
+  before_action :authenticate_user!
+
+  def new
+    @encounter = Encounter.new
+  end
+
+  def index
+    @encounters = Encounter.all
+  end
+
+  def create
+    @encounter = Encounter.new(encounter_params)
+    if @encounter.save
+      redirect_to encounters_url, notice: 'Encounter added to schedule'
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @encounter = Encounter.find(params[:id])
+  end
+
+  protected
+  def encounter_params
+    params.require(:encounter).permit(:patient_name, :insurance_provider, :notes, :procedure_id, :user_id)
+  end
 end
--- spec/features/user_adds_encounters_spec.rb
+++ spec/features/user_adds_encounters_spec.rb
@@ -1,18 +1,47 @@
 require 'spec_helper'
 
-feature 'physician can extract procedure description, code, and work RVU from file', %Q{
+feature 'physician adds procedures to a list',  %Q{
   As an authenticated physician/user
-  I want to submit procedure description
+  I want to create a new instance of a procedure
   So that I can populate my schedule with relevant procedure code and work RVU
-  } do
+} do
 
   # ACCEPTANCE CRITERIA
-  # I can submit procedure description in query
+  # I can submit procedure in search field
   # I can see closest matches to query and select exact CMS.gov descriptions
+  # I can add an existing/recent procedure from drop-down list
   # I can populate my relevant code and work RVU fields after selecting procedure description
   # I can persist submissions to the database
+  # I can see all my procedures in my daily schedule
 
-  scenario 'user adds a description for a patient encounter' do
+  let(:user) { FactoryGirl.create(:user) }
 
+  scenario 'physician inputs a valid procedure for a patient encounter' do
+    procedure = FactoryGirl.create(:procedure)
+    encounter = FactoryGirl.build(:encounter, procedure: nil)
+    prev_count = Encounter.count
+    sign_in_as(user)
+    visit new_encounter_url
+
+    fill_in 'Patient Name', with: encounter.patient_name
+    fill_in 'Insurance Provider', with: encounter.insurance_provider
+    binding.pry
+    select procedure.description, from: 'Procedure Description'
+    select procedure.code, from: 'Code'
+    select procedure.work_rvu, from: 'Work RVU'
+    fill_in 'Notes', with: encounter.notes
+    click_on 'Add Encounter'
+
+    expect(page).to have_content('Encounter added to schedule')
+    expect(Encounter.count).to eq(prev_count + 1)
   end
+
+  # scenario 'physician can review a specific procedure and notes' do
+  #   procedure = FactoryGirl.create(:procedure)
+  #   sign_in_as(user)
+  #   visit new_procedure_path
+  #   click_on 'Add Procedure'
+
+  #   expect(current_url).to eq(procedures_url)
+  # end
 end
