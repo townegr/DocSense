7b47f4c107ec2e035e95473ee2dd0706f32e7e1a
Greg Towne <townegr@yahoo.com>
2014-04-22 13:45:18 -0400

    add buttons that groups encounters by date

--- app/models/report.rb
+++ app/models/report.rb
@@ -3,13 +3,14 @@ class Report
 
   attr_reader :start_date, :end_date
 
-  def initialize(start_date = nil, end_date = nil)
-    @start_date = start_date
-    @end_date = end_date
+  def initialize(user, start_date = nil, end_date = nil)
+    @user = current_user
+    @start_date = start_date ? Date.parse(start_date) : nil
+    @end_date = start_date ? Date.parse(end_date) : nil
   end
 
   def encounters
-    @encounters = Encounter.where(created_at: start_date..end_date)
+    @encounters = Encounter.where(created_at: start_date.beginning_of_day..end_date.end_of_day, user: @user)
     # boundary case => test for this (start_date == end_date)
     # @encounters = Encounter.where(created_at: start_date <=> end_date)
     # @encounters = Encounter.where(user: current_user).where(created_at: start_date..end_date)
