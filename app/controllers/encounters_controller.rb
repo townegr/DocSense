78b897630e3d36f1f2e03db4550eee1a360c6170
Greg Towne <townegr@yahoo.com>
2014-04-22 20:43:48 -0400

    add ajax functionality for new encounters

--- app/controllers/encounters_controller.rb
+++ app/controllers/encounters_controller.rb
@@ -3,6 +3,7 @@ class EncountersController < ApplicationController
 
   def new
     @encounter = Encounter.new
+    @encounters = current_user.encounters
   end
 
   def index
@@ -10,6 +11,7 @@ class EncountersController < ApplicationController
       # report = Report.new(pass in the params)
       # @encounters = report.encounters
     # else
+      @encounter = Encounter.new
       @grouped_encounters = current_user.encounters.group_by_day(:created_at).count
       if params[:date]
         date = Date.parse(params[:date])
@@ -26,10 +28,18 @@ class EncountersController < ApplicationController
     @encounter.user_id = current_user.id
     @encounter.set_physician_fee
 
-    if @encounter.save
-      redirect_to encounters_url, notice: 'Encounter added to schedule'
-    else
-      render 'new'
+    respond_to do |format|
+      if @encounter.save
+        format.js
+      end
+    end
+  end
+
+  def destroy
+    @encounter = Encounter.find(params[:id])
+    @encounter.destroy
+    respond_to do |format|
+      format.js {render status: 200, json: {name: 'good job'}}
     end
   end
 
