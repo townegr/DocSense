c0ff5e18deb8ced94417d2ac254d97c6ebbe8cd6
Greg Towne <townegr@yahoo.com>
2014-04-27 01:21:55 -0400

    redesign of homepage

--- app/controllers/procedures_controller.rb
+++ app/controllers/procedures_controller.rb
@@ -1,29 +1,29 @@
-class ProceduresController < ApplicationController
-  before_action :authenticate_user!
+# class ProceduresController < ApplicationController
+#   # before_action :authenticate_user!
 
-  def new
-    @procedure = Procedure.new
-  end
+#   # def new
+#   #   @procedure = Procedure.new
+#   # end
 
-  def index
-    @procedures = Procedure.all
-  end
+#   # def index
+#   #   @procedures = Procedure.all
+#   # end
 
-  def create
-    @procedure = Procedure.new(procedure_params)
-    if @procedure.save
-      redirect_to procedures_url, notice: 'Procedure added to schedule'
-    else
-      render 'new'
-    end
-  end
+#   # def create
+#   #   @procedure = Procedure.new(procedure_params)
+#   #   if @procedure.save
+#   #     redirect_to procedures_url, notice: 'Procedure added to schedule'
+#   #   else
+#   #     render 'new'
+#   #   end
+#   # end
 
-  def show
-    @procedure = Procedure.find(params[:id])
-  end
+#   # def show
+#   #   @procedure = Procedure.find(params[:id])
+#   # end
 
-  private
-  def procedure_params
-    params.require(:procedure).permit(:description, :code, :work_rvu)
-  end
-end
+#   # private
+#   # def procedure_params
+#   #   params.require(:procedure).permit(:description, :code, :work_rvu)
+#   # end
+# end
